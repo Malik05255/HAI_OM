85678d9a0c86357de5936ed8febf5b941d304a35
Malik05255 <mmbbrr99@gmail.com>
2026-09-18 00:41:39 +0300

    fix: use Compose LayoutDirection from ui unit

--- app/src/main/java/com/haiom/app/MainActivity.kt
+++ app/src/main/java/com/haiom/app/MainActivity.kt
@@ -3,12 +3,12 @@ package com.haiom.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.LayoutDirection
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.unit.LayoutDirection
 import com.haiom.app.ui.HaiOmApp
 
 class MainActivity : ComponentActivity() {
