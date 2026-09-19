e7451420b23750902c372e205a8ad6e90cd2dc50
Malik05255 <mmbbrr99@gmail.com>
2026-09-20 00:55:40 +0300

    Release image generation policy cleanup as 0.10.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.haiom.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 59
-        versionName = "0.10.2"
+        versionCode = 60
+        versionName = "0.10.3"
 
         buildConfigField(
             "String",
