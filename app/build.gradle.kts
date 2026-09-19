7e2fd6cf9f02efaea598d991eeb7fe2ec4f70221
Malik05255 <mmbbrr99@gmail.com>
2026-09-19 23:46:50 +0300

    Mark mixed-direction code formatting as 0.10.0-dev9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.haiom.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 54
-        versionName = "0.10.0-dev8"
+        versionCode = 55
+        versionName = "0.10.0-dev9"
 
         buildConfigField(
             "String",
