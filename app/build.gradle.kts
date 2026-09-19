933d9308b3b0f7311895c964cd5d52e1ae7d75ec
Malik05255 <mmbbrr99@gmail.com>
2026-09-19 18:38:28 +0300

    Release 0.9.21 hidden top-right auto execute hotspot

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.haiom.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 41
-        versionName = "0.9.20"
+        versionCode = 42
+        versionName = "0.9.21"
 
         buildConfigField(
             "String",
