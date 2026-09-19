980478654c909774ea096499f183d39a703c8efe
Malik05255 <mmbbrr99@gmail.com>
2026-09-19 16:22:22 +0300

    Release 0.9.15 interaction and live code fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.haiom.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 35
-        versionName = "0.9.14"
+        versionCode = 36
+        versionName = "0.9.15"
 
         buildConfigField(
             "String",
