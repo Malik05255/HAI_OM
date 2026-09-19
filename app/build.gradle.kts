6c680a412ddba198f222a1d38fefd4a9fb611126
Malik05255 <mmbbrr99@gmail.com>
2026-09-20 00:43:03 +0300

    Add generated-image rendering support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,6 +94,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("io.coil-kt:coil-compose:2.7.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
 
