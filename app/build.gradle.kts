8f53510baa8bc41fa34ca66a0bbe5427b788e3f6
Malik05255 <mmbbrr99@gmail.com>
2026-09-19 23:35:44 +0300

    Mark centered auto prompt update as 0.10.0-dev7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.haiom.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 52
-        versionName = "0.10.0-dev6"
+        versionCode = 53
+        versionName = "0.10.0-dev7"
 
         buildConfigField(
             "String",
