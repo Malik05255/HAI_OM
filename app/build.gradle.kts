29c85cb368dbe3521d137bba70cec3d77a8ee2c1
Malik05255 <mmbbrr99@gmail.com>
2026-09-19 13:21:43 +0300

    Release 0.9.5 automatic tasks settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.haiom.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 25
-        versionName = "0.9.4"
+        versionCode = 26
+        versionName = "0.9.5"
 
         buildConfigField(
             "String",
