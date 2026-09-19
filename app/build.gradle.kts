ce04d6db31cde8389e1e5c1cc2ba9c3de5086911
Malik05255 <mmbbrr99@gmail.com>
2026-09-19 13:33:20 +0300

    Release 0.9.6 settings navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.haiom.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 26
-        versionName = "0.9.5"
+        versionCode = 27
+        versionName = "0.9.6"
 
         buildConfigField(
             "String",
