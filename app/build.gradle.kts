b631fa91011f5776228a9856b1e725f0f5c56678
Malik05255 <mmbbrr99@gmail.com>
2026-09-19 17:36:45 +0300

    Release 0.9.19 upper-right auto execute control

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.haiom.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 39
-        versionName = "0.9.18"
+        versionCode = 40
+        versionName = "0.9.19"
 
         buildConfigField(
             "String",
