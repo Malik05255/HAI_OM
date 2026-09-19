e6520639486e0af2b6de635013338ad25bf0ca03
Malik05255 <mmbbrr99@gmail.com>
2026-09-19 15:12:44 +0300

    Release 0.9.11 verified automatic execution status

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.haiom.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 31
-        versionName = "0.9.10"
+        versionCode = 32
+        versionName = "0.9.11"
 
         buildConfigField(
             "String",
