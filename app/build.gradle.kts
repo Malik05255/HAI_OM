259708db4531a87f01e348ad3dc154b430bda04d
Malik05255 <mmbbrr99@gmail.com>
2026-09-19 18:49:55 +0300

    Release 0.9.22 precise top-right hotspot

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.haiom.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 42
-        versionName = "0.9.21"
+        versionCode = 43
+        versionName = "0.9.22"
 
         buildConfigField(
             "String",
